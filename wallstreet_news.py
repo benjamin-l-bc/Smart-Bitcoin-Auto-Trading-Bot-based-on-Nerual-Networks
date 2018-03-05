083387e53bec9382c967c2c38d6fa8f119383e42
benjaminshi02003220 <346473509@qq.com>
2018-03-05 23:36:31 +0800

    wallstreet_news
    Adding sentiments of the news from wallstreet news blockchain channel

---
+++ wallstreet_news.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  5 22:29:34 2018
+
+@author: v-beshi
+"""
+
+import requests
+from bs4 import BeautifulSoup as bs
+import re
+from snownlp import SnowNLP
+jieba.add_word('比特币')
+jieba.add_word('以太坊')
+jieba.add_word('智能合约')
+jieba.add_word('虚拟币')
+jieba.add_word('区块链')
+jieba.add_word('加密货币')
+def wallstr_news():
+    user_agent='Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+    headers={'User-Agent':user_agent}
+    url='https://wallstreetcn.com/live/blockchain'
+    raw_page=requests.get(url,headers=headers)
+    page=bs(raw_page.text)
+    blockchain_news=page.find_all('div',class_="wscn-tab-pane")[1]
+    big_news=blockchain_news.find_all('div',class_='live-item score-2')
+    normal_news=blockchain_news.find_all('div',class_='live-item score-1')
+    s=0
+    count=0
+    for i in big_news:
+        text=i.find('div',class_='content-html').get_text()
+        sen=SnowNLP(text)
+        sentiment=sen.sentiments
+        s=s+2*sentiment
+        count=count+2
+    for i in normal_news:
+        text=i.find('div',class_='content-html').get_text()
+        sen=SnowNLP(text)
+        sentiment=sen.sentiments
+        s=s+sentiment
+        count=count+1
+    total_sentiment=s/count
+    return(total_sentiment)
