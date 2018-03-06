b96293d3df825cbc833545b44358a3c96d8811e5
benjaminshi02003220 <346473509@qq.com>
2018-03-06 10:09:19 +0800

    adding news_emotion

--- input_data.py
+++ input_data.py
@@ -11,12 +11,13 @@ import time
 import pandas as pd
 import bfx
 import huobi_USDT
+import wallstreet_news
 from okex2 import OKCoinFuture as ok
-mykey=ok('www.okex.com','Public Key','Private Key')
 
 
 def input_data(tt):
-    trade_data=pd.DataFrame(columns=['time','ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobi_USDT']) 
+    #输入想要抓取的分钟
+    trade_data=pd.DataFrame(columns=['time','ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobi_USDT','news_emotion']) 
     for i in range(0,tt):
         t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         ok0330=float(mykey.future_ticker('btc_usd','quarter')['ticker']['last'])
@@ -30,8 +31,9 @@ def input_data(tt):
         bfx_last_price=bfx.bfx_ticker()
         exchange_rate=float(mykey.exchange_rate()['rate'])
         huobiUSDT=float(huobi_USDT.get_usdt_price())
-        trade_data.loc[i]=[t,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT]
+        news_emotion=float(wallstr_news())
+        trade_data.loc[i]=[t,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion]
         print(trade_data)
-        time.sleep(30)
+        time.sleep(60)
     trade_data.to_csv('btc.csv')
         
