15ea1235579e8b51e24ea9a065fad6250b1e9c80
benjaminshi02003220 <346473509@qq.com>
2018-03-07 13:18:59 +0800

    adding notification on disconnect condition

--- input_data.py
+++ input_data.py
@@ -22,24 +22,27 @@ def input_data(tt):
     trade_data=pd.DataFrame(columns=['time','ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobi_USDT','news_emotion','8btc_emotion']) 
     for i in range(0,tt):
         t=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-        ok0330=float(mykey.future_ticker('btc_usd','quarter')['ticker']['last'])
-        ok_thisweek=float(mykey.future_ticker('btc_usd','this_week')['ticker']['last'])
-        bfx_bids_wall=bfx.bfx_books()['bids_wall']
-        bfx_asks_wall=bfx.bfx_books()['asks_wall']
-        bfx_total_bids=bfx.bfx_books()['total_bids']
-        bfx_total_asks=bfx.bfx_books()['total_asks']
-        bfx_buy_volumn=bfx.bfx_volumn()[0]
-        bfx_sell_volumn=bfx.bfx_volumn()[1]
-        bfx_last_price=bfx.bfx_ticker()
-        exchange_rate=float(mykey.exchange_rate()['rate'])
-        huobiUSDT=float(huobi_USDT.get_usdt_price())
-        #huobi USDT价格
-        news_emotion=float(wallstreet_news.wallstr_news())
-        #华尔街见闻区块链板块新闻情绪
-        e_btc_emotion=eight_btc_emotion.get_8btc_emotion(1)
-        #8BTC区块链论坛发帖情绪
-        trade_data.loc[i]=[t,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion,e_btc_emotion]
-        print(trade_data)
-        time.sleep(120)
+        try:
+            ok0330=float(mykey.future_ticker('btc_usd','quarter')['ticker']['last'])
+            ok_thisweek=float(mykey.future_ticker('btc_usd','this_week')['ticker']['last'])
+            bfx_bids_wall=bfx.bfx_books()['bids_wall']
+            bfx_asks_wall=bfx.bfx_books()['asks_wall']
+            bfx_total_bids=bfx.bfx_books()['total_bids']
+            bfx_total_asks=bfx.bfx_books()['total_asks']
+            bfx_buy_volumn=bfx.bfx_volumn()[0]
+            bfx_sell_volumn=bfx.bfx_volumn()[1]
+            bfx_last_price=bfx.bfx_ticker()
+            exchange_rate=float(mykey.exchange_rate()['rate'])
+            huobiUSDT=float(huobi_USDT.get_usdt_price())
+            #huobi USDT价格
+            news_emotion=float(wallstreet_news.wallstr_news())
+            #华尔街见闻区块链板块新闻情绪
+            e_btc_emotion=eight_btc_emotion.get_8btc_emotion(1)
+            #8BTC区块链论坛发帖情绪
+            trade_data.loc[i]=[t,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion,e_btc_emotion]
+            print(trade_data)
+            time.sleep(120) 
+        except:
+            print('connect error')             
     trade_data.to_csv('btc.csv')
         
