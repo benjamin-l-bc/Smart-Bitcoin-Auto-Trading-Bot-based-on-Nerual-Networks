41c2b4d3663ad826a6a72ea4c2fce9d9f39c5ff9
benjaminshi02003220 <346473509@qq.com>
2018-03-16 17:16:41 +0800

    updates

--- prediction.py
+++ prediction.py
@@ -8,9 +8,6 @@ from sklearn.decomposition import IncrementalPCA
 from sklearn.neural_network import MLPClassifier
 import pandas as pd
 from sklearn.externals import joblib
-
-import requests
-import json
 import time
 import pandas as pd
 import bfx
@@ -25,30 +22,44 @@ def test_data(tt):
     next5=joblib.load('next5.m')
     next10=joblib.load('next10.m')
     next15=joblib.load('next15.m')
-    for i in range(0,tt):
-        ok0330=float(mykey.future_ticker('btc_usd','quarter')['ticker']['last'])
-        ok_thisweek=float(mykey.future_ticker('btc_usd','this_week')['ticker']['last'])
-        bfx_bids_wall=bfx.bfx_books()['bids_wall']
-        bfx_asks_wall=bfx.bfx_books()['asks_wall']
-        bfx_total_bids=bfx.bfx_books()['total_bids']
-        bfx_total_asks=bfx.bfx_books()['total_asks']
-        bfx_buy_volumn=bfx.bfx_volumn()[0]
-        bfx_sell_volumn=bfx.bfx_volumn()[1]
-        bfx_last_price=bfx.bfx_ticker()
-        exchange_rate=float(mykey.exchange_rate()['rate'])
-        huobiUSDT=float(huobi_USDT.get_usdt_price())
-        #huobi USDT OTC Price
-        news_emotion=float(wallstreet_news.wallstr_news())
-        #Wallstreet.cn new emotion
-        if i==0:
-            raw=pd.DataFrame([[ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion]],columns=['ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobiUSDT','news_emotion'])
-        else:
-            raw2=pd.DataFrame([[ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion]],columns=['ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobiUSDT','news_emotion'])
-            raw=raw.append(raw2)
-        print(raw)
+    i=0
+    while i<=tt:
+        try:
+            DateTime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            ok0330=float(mykey.future_ticker('btc_usd','quarter')['ticker']['last'])
+            ok_thisweek=float(mykey.future_ticker('btc_usd','this_week')['ticker']['last'])
+            bfx_bids_wall=bfx.bfx_books()['bids_wall']
+            bfx_asks_wall=bfx.bfx_books()['asks_wall']
+            bfx_total_bids=bfx.bfx_books()['total_bids']
+            bfx_total_asks=bfx.bfx_books()['total_asks']
+            bfx_buy_volumn=bfx.bfx_volumn()[0]
+            bfx_sell_volumn=bfx.bfx_volumn()[1]
+            bfx_last_price=bfx.bfx_ticker()
+            exchange_rate=float(mykey.exchange_rate()['rate'])
+            huobiUSDT=float(huobi_USDT.get_usdt_price())
+            #huobi USDT价格
+            news_emotion=float(wallstreet_news.wallstr_news())
+            #华尔街见闻区块链板块新闻情绪
+            if i==0:
+                raw=pd.DataFrame([[DateTime,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion]],columns=['DateTime','ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobiUSDT','news_emotion'])
+            if (i>0)&(i<=15):
+                raw2=pd.DataFrame([[DateTime,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion]],index=[i],columns=['DateTime','ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobiUSDT','news_emotion'])
+                raw=raw.append(raw2)
+                
+            if i>15:
+                raw3=pd.DataFrame([[DateTime,ok0330,ok_thisweek,bfx_bids_wall,bfx_asks_wall,bfx_total_bids,bfx_total_asks,bfx_buy_volumn,bfx_sell_volumn,bfx_last_price,exchange_rate,huobiUSDT,news_emotion]],index=[i],columns=['DateTime','ok0330','ok_thisweek','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn','bfx_last_price','exchange_rate','huobiUSDT','news_emotion'])
+                raw=raw.append(raw3)
+                raw=raw.drop([i-16])
+            i=i+1
+            print(raw)
+        except:
+            print('connect error')
+        
+    return raw
 
     #PCA_ed_feature=pca.transform(feature)
     #next_5=next5.predict(PCA_ed_feature)
     #next_10=next10.predict(PCA_ed_feature)
     #next_15=next15.predict(PCA_ed_feature)
     #result=pd.DataFrame({'next_5':next_5,'next_10':next_10,'next_15':next_15})
+    
