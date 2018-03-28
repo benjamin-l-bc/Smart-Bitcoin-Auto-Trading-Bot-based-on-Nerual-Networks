15397543d76bc02e368d8993c4388e667f352d8a
benjaminshi02003220 <346473509@qq.com>
2018-03-28 16:52:57 +0800

    adding depth

--- prediction.py
+++ prediction.py
@@ -57,15 +57,15 @@ def test_data(tt):
                 next_5=next5.predict(PCA_ed_feature)[15]
                 next_10=next10.predict(PCA_ed_feature)[15]
                 next_15=next15.predict(PCA_ed_feature)[15]
-				if next_5+next_10+next_15>2:
-				#input auto-trading code to long
-				if next_5+next_10+next_15=0:
-				#input auto-trading code to short
-				#predict the future bitcoin price(5min/10min/15min)
-                print([next_5,next_10,next_15])
+                if(next_5)+(next_10)+(next_15)>2:
+                #trading code to long
+                      depth=mykey.future_depth('btc_usd','quarter','2')['asks']
+                      print(depth)
+                      print('buy buy buy')
+                print([next_5,next_10,next_15])                   
             i=i+1
             print(raw)
-			time.sleep(60)
+            time.sleep(60)
         except:
             print('connect error')          
     return raw
